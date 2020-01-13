6a513dc0628c294a71832999baac0f927719e60b
Aster <galaster@foxmail.com>
2020-01-14 01:07:18 +0800

    ⚔ Using Rayon to speed up simulations

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,124 @@
-use rand::prelude::*;
+use rand;
+use rand::Rng;
+
+#[no_mangle]
+pub extern "C" fn sp_single_six() -> i32 {
+    //任何一个六星
+    let mut p = 0.02;
+    let mut rng = rand::thread_rng();
+    let (mut m, mut n) = (0, 0);
+    let mut six = 0;
+    while six < 1 {
+        let r = rng.gen_range(0.0, 1.0);
+        m += 1;
+        if r > p {
+            n += 1
+        } else {
+            n = 0;
+            if r <= p * 0.5 {
+                six += 1;
+            }
+        }
+        if n <= 50 {
+            p = 0.02
+        } else {
+            p = 0.02 * (n - 50) as f32 + 0.02
+        }
+    }
+    return m;
+}
+
+#[no_mangle]
+pub extern "C" fn sp_double_six() -> i32 {
+    // 双六星
+    let mut p = 0.02;
+    let mut rng = rand::thread_rng();
+    let (mut m, mut n) = (0, 0);
+    let (mut six_1, mut six_2) = (0, 0);
+    while six_1 < 1 {
+        let r = rng.gen_range(0.0, 1.0);
+        m += 1;
+        if r > p {
+            n += 1
+        } else {
+            n = 0;
+            if r <= p * 0.5 {
+                six_1 += 1;
+            }
+        }
+
+        if n <= 50 {
+            p = 0.02
+        } else {
+            p = 0.02 * (n - 50) as f32 + 0.02
+        }
+    }
+    while six_2 < 1 {
+        let r = rng.gen_range(0.0, 1.0);
+        m += 1;
+        if r > p {
+            n += 1
+        } else {
+            n = 0;
+            if r <= p * 0.5 {
+                six_2 += 1;
+            }
+        }
+
+        if n <= 50 {
+            p = 0.02
+        } else {
+            p = 0.02 * (n - 50) as f32 + 0.02
+        }
+    }
+    return m;
+}
+
+#[no_mangle]
+pub extern "C" fn sp_double_finish() -> i32 {
+    // 双六星满潜
+    let mut p = 0.02;
+    let mut rng = rand::thread_rng();
+    let (mut m, mut n) = (0, 0);
+    let (mut six_1, mut six_2) = (0, 0);
+    while six_1 < 6 {
+        let r = rng.gen_range(0.0, 1.0);
+        m += 1;
+        if r > p {
+            n += 1
+        } else {
+            n = 0;
+            if r <= p * 0.5 {
+                six_1 += 1;
+            }
+        }
+
+        if n <= 50 {
+            p = 0.02
+        } else {
+            p = 0.02 * (n - 50) as f32 + 0.02
+        }
+    }
+    while six_2 < 6 {
+        let r = rng.gen_range(0.0, 1.0);
+        m += 1;
+        if r > p {
+            n += 1
+        } else {
+            n = 0;
+            if r <= p * 0.5 {
+                six_2 += 1;
+            }
+        }
+
+        if n <= 50 {
+            p = 0.02
+        } else {
+            p = 0.02 * (n - 50) as f32 + 0.02
+        }
+    }
+    return m;
+}
 
 #[no_mangle]
 pub extern "C" fn xd_single_six() -> i32 {
@@ -6,18 +126,16 @@ pub extern "C" fn xd_single_six() -> i32 {
     let mut p = 0.02;
     let mut rng = rand::thread_rng();
     let (mut m, mut n) = (0, 0);
-    let (mut six_1, mut six_2) = (0, 0);
-    while six_1 < 1 && six_2 < 1 {
+    let mut six = 0;
+    while six < 1 {
         let r = rng.gen_range(0.0, 1.0);
         m += 1;
         if r > p {
             n += 1
         } else {
             n = 0;
-            if r <= p * 0.35 {
-                six_1 += 1
-            } else if r <= p * 0.7 {
-                six_2 += 1
+            if r <= p * 0.7 {
+                six += 1;
             }
         }
         if n <= 50 {
@@ -35,8 +153,8 @@ pub extern "C" fn xd_select_six() -> i32 {
     let mut p = 0.02;
     let mut rng = rand::thread_rng();
     let (mut m, mut n) = (0, 0);
-    let (mut six_1, mut six_2) = (0, 0);
-    while six_1 < 1 {
+    let mut six = 0;
+    while six < 1 {
         let r = rng.gen_range(0.0, 1.0);
         m += 1;
         if r > p {
@@ -44,7 +162,7 @@ pub extern "C" fn xd_select_six() -> i32 {
         } else {
             n = 0;
             if r <= p * 0.35 {
-                six_1 += 1
+                six += 1;
             }
         }
         if n <= 50 {
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod lib;
 
-use lib::{sp_double_finish, sp_double_six, sp_single_six, xd_double_finish};
+use lib::{
+    sp_double_finish, sp_double_six, sp_single_six, xd_double_finish, xd_double_six, xd_select_six,
+    xd_single_six,
+};
 
-use rand::prelude::*;
+use rayon::prelude::*;
 use std::collections::BTreeMap;
 use std::fs::File;
 use std::io::prelude::*;
@@ -12,31 +15,28 @@ const TIMES: i32 = 1000_0000;
 
 fn main() -> std::io::Result<()> {
     let start = PreciseTime::now();
-    let mut map = BTreeMap::new();
+    let mut map: BTreeMap<String, Vec<i32>> = BTreeMap::new();
     //
-    let mut tests = vec![];
-    for _ in 1..=TIMES {
-        tests.push(xd_single_six())
-    }
-    map.insert("single_six".to_string(), tests);
+    let tests = (1..=TIMES).into_par_iter().map(|_| sp_single_six());
+    map.insert("normal_six".to_string(), tests.collect());
     //
-    let mut tests = vec![];
-    for _ in 1..=TIMES {
-        tests.push(xd_select_six())
-    }
-    map.insert("select_six".to_string(), tests);
+    let tests = (1..=TIMES).into_par_iter().map(|_| sp_double_six());
+    map.insert("normal_double".to_string(), tests.collect());
     //
-    let mut tests = vec![];
-    for _ in 1..=TIMES {
-        tests.push(xd_double_six())
-    }
-    map.insert("double_six".to_string(), tests);
+    let tests = (1..=TIMES).into_par_iter().map(|_| sp_double_finish());
+    map.insert("normal_finish".to_string(), tests.collect());
     //
-    let mut tests = vec![];
-    for _ in 1..=TIMES {
-        tests.push(xd_double_finish())
-    }
-    map.insert("double_finish".to_string(), tests);
+    let tests = (1..=TIMES).into_par_iter().map(|_| xd_single_six());
+    map.insert("single_six".to_string(), tests.collect());
+    //
+    let tests = (1..=TIMES).into_par_iter().map(|_| xd_select_six());
+    map.insert("select_six".to_string(), tests.collect());
+    //
+    let tests = (1..=TIMES).into_par_iter().map(|_| xd_double_six());
+    map.insert("double_six".to_string(), tests.collect());
+    //
+    let tests = (1..=TIMES).into_par_iter().map(|_| xd_double_finish());
+    map.insert("double_finish".to_string(), tests.collect());
     //
     let serialized = serde_pickle::to_vec(&map, true).unwrap();
     let mut file = File::create("monte_carlo.pkl")?;
